c0cb7a4d6c97563ade9795bf61f6e5b7b1f3dea0
NicolasKargruber <nicolas.kargruber@gmail.com>
2021-01-16 13:00:27 +0100

    added a Padding to the graphView of 60 random Datapoints will now be drawn into the graph

--- app/src/main/java/com/IMM2020/imm_project/SecondFragment.java
+++ app/src/main/java/com/IMM2020/imm_project/SecondFragment.java
@@ -1,6 +1,7 @@
 package com.IMM2020.imm_project;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,13 +13,17 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.Random;
+
 public class SecondFragment extends Fragment {
 
-
+    private Handler mHandler = new Handler();
     LineGraphSeries<DataPoint> series;
+    private double cur = 0;
 
     @Override
     public View onCreateView(
@@ -39,12 +44,27 @@ public class SecondFragment extends Fragment {
         double x;
 
         GraphView graphView = (GraphView) getView().findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
-                /*new DataPoint(1,1),
+
+        GridLabelRenderer glr = graphView.getGridLabelRenderer();
+        glr.setPadding(60); // should allow for 3 digits to fit on screen
+
+        series = new LineGraphSeries<>(new DataPoint[]{
+                new DataPoint(1,1),
                 new DataPoint(2,2),
-                new DataPoint(3,1)*/
+                new DataPoint(3,1)
         });
         graphView.addSeries(series);
+
+        graphView.getViewport().setMinX(0);
+        graphView.getViewport().setMaxX(1000);
+        graphView.getViewport().setXAxisBoundsManual(true);
+        graphView.getViewport().setMinY(0);
+        graphView.getViewport().setMaxY(10);
+        graphView.getViewport().setYAxisBoundsManual(true);
+
+        graphView.getViewport().setScalable(true);
+
+        addDataPoints();
 
         view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +74,16 @@ public class SecondFragment extends Fragment {
             }
         });
         //Toast.makeText(view.getContext(), "HELLO WORLD", Toast.LENGTH_LONG).show();
+    }
 
+    private void addDataPoints(){
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                cur+=10;
+                series.appendData(new DataPoint(cur, new Random().nextInt(10)), false, 200);
+                if(cur<=1000){addDataPoints();}
+            }
+        }, 100);
     }
 }
