33839275b7a978cad28ea03dbae07452b916684a
NicolasKargruber <nicolas.kargruber@gmail.com>
2021-01-17 19:40:23 +0100

    Switched First fragment and Second Fragment to show up first

--- app/src/main/java/com/IMM2020/imm_project/MainActivity.java
+++ app/src/main/java/com/IMM2020/imm_project/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);*/
 
         /*FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickL istener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
