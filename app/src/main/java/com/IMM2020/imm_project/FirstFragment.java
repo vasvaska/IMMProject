40cd242e3898a2c22cd684443f0928e1922e63ac
Anna Shi <anna.shiganyan@hotmail.com>
2021-01-17 14:55:49 +0100

    Its me, Mario

--- app/src/main/java/com/IMM2020/imm_project/FirstFragment.java
+++ app/src/main/java/com/IMM2020/imm_project/FirstFragment.java
@@ -1,15 +1,32 @@
 package com.IMM2020.imm_project;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class FirstFragment extends Fragment {
+import java.util.Random;
+
+public class FirstFragment extends Fragment implements View.OnClickListener   {
+    Button btn_one, btn_two, btn_three, btn_four;
+    TextView tv_question;
+
+    private Question question = new Question();
+
+    private String answer;
+    private int questionLength = question.questions.length;
+
+    Random random;
 
     @Override
     public View onCreateView(
@@ -20,6 +37,82 @@ public class FirstFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_one:
+                if(btn_one.getText() == answer){
+                    Toast.makeText(FirstFragment.this, "You Are Correct", Toast.LENGTH_SHORT).show();
+                    NextQuestion(random.nextInt(questionLength));
+                }else{
+                    GameOver();
+                }
+
+                break;
+
+            case R.id.btn_two:
+                if(btn_two.getText() == answer){
+                    Toast.makeText(FirstFragment.this, "You Are Correct", Toast.LENGTH_SHORT).show();
+                    NextQuestion(random.nextInt(questionLength));
+                }else{
+                    GameOver();
+                }
+
+                break;
+
+            case R.id.btn_three:
+                if(btn_three.getText() == answer){
+                    Toast.makeText(FirstFragment.this, "You Are Correct", Toast.LENGTH_SHORT).show();
+                    NextQuestion(random.nextInt(questionLength));
+                }else{
+                    GameOver();
+                }
+
+                break;
+
+            case R.id.btn_four:
+                if(btn_four.getText() == answer){
+                    Toast.makeText(FirstFragment.this, "You Are Correct", Toast.LENGTH_SHORT).show();
+                    NextQuestion(random.nextInt(questionLength));
+                }else{
+                    GameOver();
+                }
+
+                break;
+        }
+    }
+
+    private void GameOver(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(FirstFragment.this);
+        alertDialogBuilder
+                .setMessage("Game Over")
+                .setCancelable(false)
+                .setPositiveButton("New Game", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(getApplicationContext(), FirstFragment.class));
+                    }
+                })
+                .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        System.exit(0);
+                    }
+                });
+        alertDialogBuilder.show();
+
+    }
+
+
+    private void NextQuestion(int num){
+        tv_question.setText(question.getQuestion(num));
+        btn_one.setText(question.getchoice1(num));
+        btn_two.setText(question.getchoice2(num));
+        btn_three.setText(question.getchoice3(num));
+        btn_four.setText(question.getchoice4(num));
+
+        answer = question.getCorrectAnswer(num);
+    }
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
