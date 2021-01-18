536051526bb6d753060b95377d6ca19787bb5b8e
Vasil Veselinov Vodenicharov <vasvaska@live.com>
2021-01-18 15:41:53 +0000

    Fixed questions. Need to add more answers for 2 questions.

--- app/src/main/java/com/IMM2020/imm_project/FirstFragment.java
+++ app/src/main/java/com/IMM2020/imm_project/FirstFragment.java
@@ -17,16 +17,17 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import java.util.Random;
 
-public class FirstFragment extends Fragment implements View.OnClickListener   {
+public class FirstFragment extends Fragment {
     Button btn_one, btn_two, btn_three, btn_four;
     TextView tv_question;
 
-    private Question question = new Question();
+    private Question QuestionList = new Question(this.getContext());
 
     private String answer;
-    private int questionLength = question.questions.length;
+    private int questionLength = QuestionList.questions.length;
 
-    Random random;
+    Random random = new Random();
+
 
     @Override
     public View onCreateView(
@@ -34,63 +35,70 @@ public class FirstFragment extends Fragment implements View.OnClickListener   {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_first, container, false);
+
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn_one:
-                if(btn_one.getText() == answer){
-                    Toast.makeText(getContext(), "You Are Correct", Toast.LENGTH_SHORT).show();
-                    NextQuestion(random.nextInt(questionLength));
-                }else{
-                    GameOver();
-                }
 
-                break;
+    private View.OnClickListener clicker = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
 
-            case R.id.btn_two:
-                if(btn_two.getText() == answer){
-                    Toast.makeText(getContext(), "You Are Correct", Toast.LENGTH_SHORT).show();
-                    NextQuestion(random.nextInt(questionLength));
-                }else{
-                    GameOver();
-                }
 
-                break;
+            switch (v.getId()) {
+                case R.id.btn_one:
+                    if (btn_one.getText() == answer) {
+                        Toast.makeText(getContext(), R.string.correctAnswer, Toast.LENGTH_SHORT).show();
+                        NextQuestion(random.nextInt(questionLength));
+                    } else {
+                        GameOver();
+                    }
 
-            case R.id.btn_three:
-                if(btn_three.getText() == answer){
-                    Toast.makeText(getContext(), "You Are Correct", Toast.LENGTH_SHORT).show();
-                    NextQuestion(random.nextInt(questionLength));
-                }else{
-                    GameOver();
-                }
+                    break;
 
-                break;
+                case R.id.btn_two:
+                    if (btn_two.getText() == answer) {
+                        Toast.makeText(getContext(), R.string.correctAnswer, Toast.LENGTH_SHORT).show();
+                        NextQuestion(random.nextInt(questionLength));
+                    } else {
+                        GameOver();
+                    }
 
-            case R.id.btn_four:
-                if(btn_four.getText() == answer){
-                    Toast.makeText(getContext(), "You Are Correct", Toast.LENGTH_SHORT).show();
-                    NextQuestion(random.nextInt(questionLength));
-                }else{
-                    GameOver();
-                }
+                    break;
 
-                break;
+                case R.id.btn_three:
+                    if (btn_three.getText() == answer) {
+                        Toast.makeText(getContext(), R.string.correctAnswer, Toast.LENGTH_SHORT).show();
+                        NextQuestion(random.nextInt(questionLength));
+                    } else {
+                        GameOver();
+                    }
+
+                    break;
+
+                case R.id.btn_four:
+                    if (btn_four.getText() == answer) {
+                        Toast.makeText(getContext(), R.string.correctAnswer, Toast.LENGTH_SHORT).show();
+                        NextQuestion(random.nextInt(questionLength));
+                    } else {
+                        GameOver();
+                    }
+
+                    break;
+            }
         }
-    }
+    };
 
-    private void GameOver(){
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
+    private void GameOver() {
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
         alertDialogBuilder
-                .setMessage("Game Over")
+                .setMessage("Wrong answer. Correct was: " + answer)
                 .setCancelable(false)
-                .setPositiveButton("New Game", new DialogInterface.OnClickListener() {
+                .setPositiveButton("Try again", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        startActivity(new Intent(getContext(), FirstFragment.class));
+                        NextQuestion(random.nextInt(questionLength));
                     }
                 })
                 .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
@@ -104,26 +112,40 @@ public class FirstFragment extends Fragment implements View.OnClickListener   {
     }
 
 
-    private void NextQuestion(int num){
-        tv_question.setText(question.getQuestion(num));
-        btn_one.setText(question.getchoice1(num));
-        btn_two.setText(question.getchoice2(num));
-        btn_three.setText(question.getchoice3(num));
-        btn_four.setText(question.getchoice4(num));
+    private void NextQuestion(int num) {
+        tv_question.setText(QuestionList.getQuestion(num));
+        btn_one.setText(QuestionList.getchoice1(num));
+        btn_two.setText(QuestionList.getchoice2(num));
+        btn_three.setText(QuestionList.getchoice3(num));
+        btn_four.setText(QuestionList.getchoice4(num));
 
-        answer = question.getCorrectAnswer(num);
+        answer = QuestionList.getCorrectAnswer(num);
     }
+
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        random.nextInt();
+        btn_one = view.findViewById(R.id.btn_one);
+        btn_one.setOnClickListener(clicker);
 
+        btn_two = view.findViewById(R.id.btn_two);
+        btn_two.setOnClickListener(clicker);
+
+        btn_three = view.findViewById(R.id.btn_three);
+        btn_three.setOnClickListener(clicker);
+
+        btn_four = view.findViewById(R.id.btn_four);
+        btn_four.setOnClickListener(clicker);
+        tv_question = view.findViewById(R.id.tv_question);
+        NextQuestion(random.nextInt(questionLength));
         view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
-            //boop boop
-            //IMMPROJEKT
+
         });
+
     }
 }
