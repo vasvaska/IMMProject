acefadaa4233717b0f5ceed6c7e7a7d7b09e7749
vasvaska <vasvaska@gmail.com>
2021-01-20 16:05:30 +0000

    BIG UI changes. save your changes locally and resync.

---
+++ app/src/main/java/com/IMM2020/imm_project/SectionsPagerAdapter.java
@@ -0,0 +1,46 @@
+package com.IMM2020.imm_project;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    @StringRes
+    //private static final int[] TAB_TITLES = new int[]{R.string.app_name, R.string.app_name};
+    private static final String[] TAB_TITLES = new String[]{"EMG Data", "Quiz"};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        if (position == 0)
+            return FirstFragment.newInstance();
+        else
+            return SecondFragment.newInstance();
+
+        //return SecondFragment.newInstance(position + 1);
+
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return TAB_TITLES[position];
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 2;
+    }
+}
